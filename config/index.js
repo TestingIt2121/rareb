7691286ceb70ea03042e3d02f8f6b2d167e51ad7
TestingIt2121 <112033294+TestingIt2121@users.noreply.github.com>
2022-10-04 12:55:05 +0100

    Add files via upload

---
+++ config/index.js
@@ -0,0 +1,15 @@
+export const config = {
+  env: "prod",
+  LOCAL_API_URL: "fbg-mu.vercel.app",
+  API_URL: "fbg-mu.vercel.app",
+  COLLECTION_NAME: "Fireball Genesis",
+  COLLECTION_TITLE: "Fireball Genesis Collection",
+  COLLECTION_DESCRIPTION: 'Introducing Fireball Genesis Collection. The origin story starts here. Fireball has defied all odds in this bloody market! These NFT’s represent Fireball in his many forms. The conqueror. The warrior. The diamond collector.',
+  COLLECTION_IMG_LINK: "https://finu.farm/fdgc/0/embed/0000.png",
+  CONTRACT: {
+    // if you're not sure, set CONTRACT: {}, will remove opensea link
+    //CHAIN: "", //possible values of ethereum, matic (polygin), klatyn, solana
+    //ADDRESS: "", //note: for solana these values change, so currently not supported
+  },
+  WEIGHTS: {},
+};
